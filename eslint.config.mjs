dab7f66c4696c3f74db6e66e44503d6c69e6b5ae
cevheri <cevheribozoglan@gmail.com>
2026-05-13 15:52:14 +0300

    chore: eslint + prettier for astro

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import eslintPluginAstro from 'eslint-plugin-astro';
+
+export default [
+  ...eslintPluginAstro.configs.recommended,
+  {
+    ignores: ['dist/', '.astro/', 'node_modules/'],
+  },
+  {
+    rules: {
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+  },
+];
